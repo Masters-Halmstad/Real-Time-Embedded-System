a80bb9f02fa8187f5133b06b3c84c56e326cda10
Binay Sah <sahbinay432@gmail.com>
2025-09-19 10:49:54 +0200

    Complete Assignment 1 part 2

---
+++ a1p2.c
@@ -0,0 +1,24 @@
+/*
+    Part of the Real-Time Embedded Systems course at Halmstad University
+    Wagner de Morais (Wagner.deMorais@hh.se)
+*/
+
+#include "led.h"
+#include "rpi3.h"
+#include "rpi-systimer.h"
+#include "rpi-gpio.h"
+
+int main()
+{
+	led_init();
+
+    //blinking
+    while(1){
+        led_blink();
+    }
+
+    //toogle 
+    // led_off();
+    // RPI_WaitMicroSeconds(100000);
+    // led_toogle();
+}
