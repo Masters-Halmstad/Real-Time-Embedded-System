aedf6fe730219a87c631f4e8533e6ce63581f1c0
Binay Sah <sahbinay432@gmail.com>
2025-09-21 22:38:07 +0200

    V1.0 forced and impractical way to achieve delay

--- a2p1.c
+++ a2p1.c
@@ -5,12 +5,26 @@
 #include <stdio.h>
 #include "piface.h"
 
+#define LCD_LONG_DELAY \
+	do { for(volatile int DELAYx = 0; DELAYx < 5000000; DELAYx++); } while(0);
+
+
 int main()
 {
 	piface_init();
 	piface_clear();
-	piface_puts("Short string");
-	piface_puts("This is a long string");
-	piface_puts("1st line\n2nd line");
+	while(1){
+		piface_clear();
+		piface_puts("Short string");
+		LCD_LONG_DELAY;
+
+		piface_clear();
+		piface_puts("This is a long string");
+		LCD_LONG_DELAY;
+
+		piface_clear();
+		piface_puts("1st line\n2nd line");
+		LCD_LONG_DELAY;
+	}
 	return 0;
 }
