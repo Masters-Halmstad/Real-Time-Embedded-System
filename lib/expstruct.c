8286bd04cf47f3823e4650bc1efc598dfcf3534a
Binay Sah <sahbinay432@gmail.com>
2025-09-23 12:22:43 +0200

    added iexp function definition in expstruct.c

--- lib/expstruct.c
+++ lib/expstruct.c
@@ -12,6 +12,31 @@
 #include "expstruct.h"
 
 ExpStruct *iexp(int x){
+    // check pre-condition 
+    if (x<0 || x>20){
+        fprintf(stderr, "Error: Entered number should be between 0 to 20 \n");
+        return NULL;
+    }
+
+    double factorialSum = 0;  
+    double termValue = 1.0;  
+    int interation = 0; 
+
+    while (iteration < 100) {
+        termValue *= x/n ; // termValue  = (x/n) * (x^(n-1) / ((n-1)!))
+        factorialSum += termValue;
+
+        // if new term is small enough that will not effect the result(sum) beyond two decimal place stop the iteration 
+        if(termValue < 0.001){
+            break;
+        }
+
+        iteration++;
+    }
+
 	ExpStruct *e = malloc(sizeof(ExpStruct));
+    e->expInt = (int) floor(factorialSum);
+    e->expFraction = (int)((factorialSum - expInt)*100);
+    
 	return e;	
 }
