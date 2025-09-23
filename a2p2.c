bcf96c058ee7d66065ee25e92436cd7b44c0f252
Binay Sah <sahbinay432@gmail.com>
2025-09-23 13:23:35 +0200

    fixed lcd display and iexp function

--- a2p2.c
+++ a2p2.c
@@ -1,36 +1,38 @@
 /*
-    Part of the Real-Time Embedded Systems course at Halmstad University
-    Copyright (c) 2017, Sebastian Kunze <sebastian.kunze@hh.se>
-    All rights reserved.
+	Part of the Real-Time Embedded Systems course at Halmstad University
+	Copyright (c) 2017, Sebastian Kunze <sebastian.kunze@hh.se>
+	All rights reserved.
 	Wagner de Morais (Wagner.deMorais@hh.se)
 */
 
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "expstruct.h"
 #include "piface.h"
+#include <string.h>
 
 #define LINE 32
+#define waitTime 2000000
 
 int main()
 {
-	
+
 	char str[LINE];
 	piface_init();
 	piface_clear();
-	
+
 	piface_puts("DT8025 - A2P2");
-	RPI_WaitMicroSeconds(2000000);	
-    piface_clear();
+	RPI_WaitMicroSeconds(waitTime);
+	ExpStruct *value;
 
-    ExpStruct* value;
-    
-    value = iexp(10);
-	
-    sprintf(str,"%d: %d.%d", 10, value->expInt, value->expFraction);
+	value = iexp(10);
+
+	sprintf(str, "%d: %d.%d", 10, value->expInt, value->expFraction);
+	// strcpy(str, "10: 22026.47");
+	piface_clear();
+	RPI_WaitMicroSeconds(waitTime);
 	piface_puts(str);
+	RPI_WaitMicroSeconds(waitTime);
 	free(value);
-
 	return 0;
-
 }
--- lib/expstruct.c
+++ lib/expstruct.c
@@ -6,37 +6,43 @@
 /*
  * Modified by Wagner Morais on Aug 2022.
  */
- 
+
 #include <stddef.h>
 #include <stdlib.h>
 #include "expstruct.h"
+#include <math.h>
+#include <stdio.h>
 
-ExpStruct *iexp(int x){
-    // check pre-condition 
-    if (x<0 || x>20){
+ExpStruct *iexp(int x)
+{
+    // check pre-condition
+    if (x < 0 || x > 20)
+    {
         fprintf(stderr, "Error: Entered number should be between 0 to 20 \n");
         return NULL;
     }
 
-    double factorialSum = 0;  
-    double termValue = 1.0;  
-    int interation = 0; 
+    double factorialSum = 1.0;
+    double termValue = 1.0;
+    int n = 1; // iteration
 
-    while (iteration < 100) {
-        termValue *= x/n ; // termValue  = (x/n) * (x^(n-1) / ((n-1)!))
+    while (n < 100)
+    {
+        termValue *= (double)x / n; // termValue  = (x/n) * (x^(n-1) / ((n-1)!))
         factorialSum += termValue;
 
-        // if new term is small enough that will not effect the result(sum) beyond two decimal place stop the iteration 
-        if(termValue < 0.001){
+        // if new term is small enough that will not effect the result(sum) beyond two decimal place stop the iteration
+        if (termValue < 0.001)
+        {
             break;
         }
 
-        iteration++;
+        n++;
     }
 
-	ExpStruct *e = malloc(sizeof(ExpStruct));
-    e->expInt = (int) floor(factorialSum);
-    e->expFraction = (int)((factorialSum - expInt)*100);
-    
-	return e;	
+    ExpStruct *e = (ExpStruct *)malloc(sizeof(ExpStruct));
+    e->expInt = (int)factorialSum;
+    e->expFraction = (int)((factorialSum - (e->expInt)) * 100 + 0.5);
+
+    return e;
 }
